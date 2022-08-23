e6f47c9bb8754d5b2e0941637ba731a83ad32af5
Andrew Kigen <andrew.kigen@student.moringaschool.com>
2022-08-23 08:34:51 +0300

    completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
 // code your solution here
+const record = [    { year: "2015", result: "W"},    { year: "2014", result: "N/A"},    { year: "2013", result: "L"},]
+function superbowlWin(array) {    let res ;    for( const benv of array) {        if(benv.result === 'W') {           
+     res =array.find(answer).year;          
+     return res        }    }}
+function answer ( item,) {    
+    return item.result==="W"}
